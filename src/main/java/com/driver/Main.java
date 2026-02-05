592148389870df12ae305b22e3bc926cb29c6764
sukanyasahu123 <sukanya.sahu56@gmail.com>
2026-02-05 22:23:09 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,17 @@
 package com.driver;
-
+class Animal {
+    void sound() {
+        System.out.println("Animal makes a sound");
+    }
+}
+class Dog extends Animal {
+    void sound() {
+        System.out.println("Dog barks");
+    }
+}
 public class Main {
-
+    public static void main(String[] args) {
+        Animal a = new Dog();
+        a.sound();
+    }
 }
